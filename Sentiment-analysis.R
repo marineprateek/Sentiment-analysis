27d38367fe385d4e6857fdbdff1b0002e10c93ba
marineprateek <prateekrlinsbits@gmail.com>
2020-06-10 10:58:05 +0530

    added the mutate and spread function

--- Sentiment-analysis.R
+++ Sentiment-analysis.R
@@ -1,26 +1,25 @@
 library(tidytext) #tidytext package contains sentiments lexicons
 get_sentiments("bing")
 library(janeaustenr)
-library(stringr) #stringr package lets us use the pipe operator(%>%) 
+library(stringr) #stringr package lets us use the pipe operator(%>%)
+library(dplyr) #dplyr package lets us use group_by function
 tidy_data<- austen_books()%>%group_by(book)%>% #austen_books() to gets a tibble of  books
   mutate(linenumber=row_number(),
-         chapter=cumsum(str_detect(text,regex("^chapter [\\divxcl]", #\\d includes all decimal digits
-                                              ignore_case = T))))%>% #ignorecase considers both small and big case
+         chapter=cumsum(str_detect(text,regex("^chapter [\\divxcl]",
+                                              ignore_case = T))))%>%
 ungroup()%>%
   unnest_tokens(word,text)
-positive_senti<- get_sentiments("bing")%>%filter(sentiment=="positive") #creating a tibble of all the 
-#positive words from bing sentiment
+positive_senti<- get_sentiments("bing")%>%filter(sentiment=="positive")
 positivetidy_data<- tidy_data%>%filter(book=="Sense & Sensibility")%>%semi_join(positive_senti)%>%count(word,sort=T) #filter the 
 # tidy data created by selecting only positive words from chapter sense and sensibility
 library(tidyr)
 bing <- get_sentiments("bing")
-Senseandsensibility_sentiment<- tidy_data %>% inner_join(bing)%>%
-count(book = "Sense & Sensibility" , index = linenumber %/% 80, sentiment) %>%
-  spread(sentiment, n, fill = 0) %>%
-  mutate(sentiment = positive - negative)
+Senseandsensibility_sentiment<- tidy_data%>% filter(book=="Sense & Sensibility")%>%
+inner_join(bing)%>%count(book = "Sense & Sensibility",index = linenumber %/% 80, sentiment)%>%
+spread(sentiment, n, fill = 0) %>%mutate(sentiment = positive - negative)
 library(ggplot2)
-ggplot(Senseandsensibility_sentiment, aes(index, sentiment, fill = book)) #visualize the words
-#present in the book “Sense & Sensibility” based on their corrosponding positive and negative scores.
+ggplot(Senseandsensibility_sentiment, aes(index, sentiment, fill = book))+ #visualize the words
+#present in the book Sense & Sensibility based on their corrosponding positive and negative scores.
   geom_bar(stat = "identity", show.legend = TRUE) +
   facet_wrap(~book, ncol = 2, scales = "free_x")
   
